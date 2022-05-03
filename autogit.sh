82789deab26386934e987262ef2a47b27f03ae73
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-05-02 23:22:38 -0300

    NLW Return -> started

---
+++ autogit.sh
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "NLW Return -> started"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/NLW-Return.git
+git push -u origin main
+git status
